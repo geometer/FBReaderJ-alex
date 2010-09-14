ad1f2d8ba20f17571f6e732bc7996bff2dbeae67
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-09-14 11:02:14 +0000

    Minor bugfixes: external books from archives in the Library (Merge r1743)
    git-svn-id: https://only.mawhrin.net/repos/FBReaderJ/branches/alex@1746 6a642e6f-84f6-412e-ac94-c4a38d5a04b0

--- src/org/geometerplus/fbreader/fbreader/FBReader.java
+++ src/org/geometerplus/fbreader/fbreader/FBReader.java
@@ -238,6 +238,7 @@ public final class FBReader extends ZLApplication {
 			for (ZLFile child : file.children()) {
 				book = Book.getByFile(child);
 				if (book != null) {
+					book.insertIntoBookList();
 					return book;
 				}
 			}
