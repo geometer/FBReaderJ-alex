5479c326d3184c8aa38e0c19d62f177f9e5c304d
Nikolay Pultsin <geometer@fbreader.org>
2010-11-13 17:13:15 +0000

    support for multiple network catalogs has been implemented
    (cherry picked from commit d1f6a9eab790daf15bb53d0c84b1d2d9823663e3 with conflicts)

--- src/org/geometerplus/android/fbreader/network/NetworkLibraryActivity.java
+++ src/org/geometerplus/android/fbreader/network/NetworkLibraryActivity.java
@@ -33,6 +33,7 @@ import android.widget.BaseAdapter;
 
 import org.geometerplus.zlibrary.core.resources.ZLResource;
 import org.geometerplus.zlibrary.core.network.ZLNetworkException;
+import org.geometerplus.zlibrary.core.language.ZLLanguageUtil;
 
 import org.geometerplus.zlibrary.ui.android.R;
 
@@ -233,6 +234,7 @@ public class NetworkLibraryActivity extends NetworkBaseActivity {
 	private static final int MENU_SEARCH = 1;
 	private static final int MENU_REFRESH = 2;
 	private static final int MENU_ADD_CATALOG = 3;
+	private static final int MENU_LANGUAGES = 4;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -240,6 +242,7 @@ public class NetworkLibraryActivity extends NetworkBaseActivity {
 		addMenuItem(menu, MENU_SEARCH, "networkSearch", R.drawable.ic_menu_networksearch);
 		addMenuItem(menu, MENU_ADD_CATALOG, "addCustomCatalog", android.R.drawable.ic_menu_add);
 		addMenuItem(menu, MENU_REFRESH, "refreshCatalogsList", R.drawable.ic_menu_refresh);
+		addMenuItem(menu, MENU_LANGUAGES, "languages", 0);
 		return true;
 	}
 
@@ -261,6 +264,11 @@ public class NetworkLibraryActivity extends NetworkBaseActivity {
 				return true;
 			case MENU_REFRESH:
 				refreshCatalogsList();
+				return true;
+			case MENU_LANGUAGES:
+				for (String langCode : NetworkLibrary.Instance().languages()) {
+					System.err.println("Language: " + ZLLanguageUtil.languageName(langCode));
+				}
 				return true;
 			default:
 				return true;
