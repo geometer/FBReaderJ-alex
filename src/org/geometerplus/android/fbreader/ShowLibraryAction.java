338443984b4609774d91219ce9d7087734d1a6a5
Nikolay Pultsin <geometer@fbreader.org>
2010-11-17 20:56:03 +0000

    ChangeLog has been updated waiting dialog has been moved from ShowLibraryAction to LibraryTabActivity (cherry picked from commit 6ec85ec8ce6329791adabc31ed2d60e9f76efa3c)

--- src/org/geometerplus/android/fbreader/ShowLibraryAction.java
+++ src/org/geometerplus/android/fbreader/ShowLibraryAction.java
@@ -25,8 +25,6 @@ import org.geometerplus.fbreader.fbreader.FBAction;
 import org.geometerplus.fbreader.fbreader.FBReaderApp;
 import org.geometerplus.fbreader.bookmodel.BookModel;
 
-import org.geometerplus.android.util.AndroidUtil;
-
 class ShowLibraryAction extends FBAction {
 	private final FBReader myBaseActivity;
 
@@ -37,15 +35,10 @@ class ShowLibraryAction extends FBAction {
 
 	public void run() {
 		final BookModel model = Reader.Model;
-		Runnable action = new Runnable() {
-			public void run() {
-				Intent intent = new Intent(myBaseActivity.getApplicationContext(), LibraryTabActivity.class);
-				if (model != null && model.Book != null) {
-					intent.putExtra(LibraryTabActivity.CURRENT_BOOK_PATH_KEY, model.Book.File.getPath());
-				}
-				myBaseActivity.startActivity(intent);
-			}
-		};
-		AndroidUtil.wait("loadingBookList", action, myBaseActivity);
+		Intent intent = new Intent(myBaseActivity.getApplicationContext(), LibraryTabActivity.class);
+		if (model != null && model.Book != null) {
+			intent.putExtra(LibraryTabActivity.CURRENT_BOOK_PATH_KEY, model.Book.File.getPath());
+		}
+		myBaseActivity.startActivity(intent);
 	}
 }
