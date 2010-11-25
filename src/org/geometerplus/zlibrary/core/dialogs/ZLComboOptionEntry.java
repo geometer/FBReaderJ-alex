ea0c1ada629d21569c82809e599f35ada1824093
Nikolay Pultsin <geometer@fbreader.org>
2010-11-25 18:59:16 +0000

    unused code has been removed
    (cherry picked from commit 4ac91f1f681ec29813ee6c967567aca384e8fc29 with conflicts)

--- src/org/geometerplus/zlibrary/core/dialogs/ZLComboOptionEntry.java
+++ src/org/geometerplus/zlibrary/core/dialogs/ZLComboOptionEntry.java
@@ -23,42 +23,38 @@ import java.util.*;
 
 public abstract class ZLComboOptionEntry extends ZLOptionEntry {
 	private final boolean myEditable;
-	
+
 	protected ZLComboOptionEntry() {
 		myEditable = false;
 	}
-	
+
 	protected ZLComboOptionEntry(boolean editable) {
 		myEditable = editable;
 	}
-	
-	public int getKind() {
-		return ZLOptionKind.COMBO;
-	}
-	
+
 	public void onValueSelected(int index) {}
-	
+
 	public final void onStringValueSelected(String value) {
 		int index = getValues().indexOf(value);
 		if (index != -1) {
 			onValueSelected(index);
 		}
 	}
-	
+
 	public boolean useOnValueEdited() {
 		return false;
 	}
-	
+
 	public void onValueEdited(String value) {}
-	
+
 	public final boolean isEditable() {
 		return myEditable;
 	}
-	
+
 	public abstract String initialValue();
-	
+
 	public abstract ArrayList<String> getValues();
-	
+
 	public abstract void onAccept(String value);
 
 	public void onReset() {
