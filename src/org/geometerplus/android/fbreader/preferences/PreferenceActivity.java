a4accc08fff47086caf01aca4105bfc85fcb6525
Nikolay Pultsin <geometer@fbreader.org>
2010-11-27 13:53:42 +0000

    more footer options
    (cherry picked from commit 2f0fe6ef820eba1b4f077ebbb1f34c9ef88b5453 with conflicts)

--- src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
+++ src/org/geometerplus/android/fbreader/preferences/PreferenceActivity.java
@@ -45,6 +45,7 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 	protected void init(Intent intent) {
 		final FBReaderApp fbReader = (FBReaderApp)FBReaderApp.Instance();
 		//final ZLAndroidApplication androidApp = ZLAndroidApplication.Instance();
+		final ColorProfile profile = fbReader.getColorProfile();
 
 		final Screen directoriesScreen = createPreferenceScreen("directories");
 		directoriesScreen.addPreference(new ZLStringOptionPreference(
@@ -214,7 +215,6 @@ public class PreferenceActivity extends ZLPreferenceActivity {
 		}
 
 		final Screen colorsScreen = createPreferenceScreen("colors");
-		final ColorProfile profile = fbReader.getColorProfile();
 		colorsScreen.addPreference(new ZLColorPreference(
 			this, colorsScreen.Resource, "background", profile.BackgroundOption
 		));
