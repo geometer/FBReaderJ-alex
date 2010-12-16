geometer/FBReaderJ

book deleting in library view has been implemented

src/org/geometerplus/android/fbreader/library/LibraryBaseActivity.java
@@ -22,6 +22,7 @@ package org.geometerplus.android.fbreader.library;
 import java.util.List;
 
 import android.app.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.*;
@@ -65,12 +66,25 @@ abstract class LibraryBaseActivity extends ListActivity {
 	protected final ZLResource myResource = ZLResource.resource("libraryView");
 	protected String mySelectedBookPath;
 
+	private static int CHILD_LIST_REQUEST = 0;
+	private static int RESULT_DONT_INVALIDATE_VIEWS = 0;
+	private static int RESULT_INVALIDATE_VIEWS = 1;
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
+		setResult(RESULT_DONT_INVALIDATE_VIEWS);
 		Thread.setDefaultUncaughtExceptionHandler(new org.geometerplus.zlibrary.ui.android.library.UncaughtExceptionHandler(this));
 		mySelectedBookPath = getIntent().getStringExtra(SELECTED_BOOK_PATH_KEY);
 	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int returnCode, Intent intent) {
+		if (requestCode == CHILD_LIST_REQUEST && returnCode == RESULT_INVALIDATE_VIEWS) {
+			getListView().invalidateViews();
+			setResult(RESULT_INVALIDATE_VIEWS);
+		}
+	} 
 
 	@Override
 	public boolean onSearchRequested() {
@@ -224,24 +238,53 @@ abstract class LibraryBaseActivity extends ListActivity {
 		final int position = ((AdapterView.AdapterContextMenuInfo)item.getMenuInfo()).position;
 		final FBTree tree = ((LibraryAdapter)getListAdapter()).getItem(position);
 		if (tree instanceof BookTree) {
-			final BookTree bookTree = (BookTree)tree;
+			final Book book = ((BookTree)tree).Book;
 			switch (item.getItemId()) {
 				case OPEN_BOOK_ITEM_ID:
-					openBook(bookTree.Book);
+					openBook(book);
 					return true;
 				case ADD_TO_FAVORITES_ITEM_ID:
-					Library.addBookToFavorites(bookTree.Book);
+					Library.addBookToFavorites(book);
 					return true;
 				case REMOVE_FROM_FAVORITES_ITEM_ID:
-					Library.removeBookFromFavorites(bookTree.Book);
+					Library.removeBookFromFavorites(book);
 					getListView().invalidateViews();
 					return true;
 				case DELETE_BOOK_ITEM_ID:
-					// TODO: implement
+					tryToDeleteBook(book);
 					return true;
 			}
 		}
 		return super.onContextItemSelected(item);
+	}
+
+	private class BookDeleter implements DialogInterface.OnClickListener {
+		private final Book myBook;
+		private final int myMode;
+
+		BookDeleter(Book book, int removeMode) {
+			myBook = book;
+			myMode = removeMode;
+		}
+
+		public void onClick(DialogInterface dialog, int which) {
+			Library.removeBook(myBook, myMode);
+			getListView().invalidateViews();
+			setResult(RESULT_INVALIDATE_VIEWS);
+		}
+	}
+
+	private void tryToDeleteBook(Book book) {
+		final ZLResource dialogResource = ZLResource.resource("dialog");
+		final ZLResource buttonResource = dialogResource.getResource("button");
+		final ZLResource boxResource = dialogResource.getResource("deleteBookBox");
+		new AlertDialog.Builder(this)
+			.setTitle(book.getTitle())
+			.setMessage(boxResource.getResource("message").getValue())
+			.setIcon(0)
+			.setPositiveButton(buttonResource.getResource("yes").getValue(), new BookDeleter(book, Library.REMOVE_FROM_DISK))
+			.setNegativeButton(buttonResource.getResource("no").getValue(), null)
+			.create().show();
 	}
 
 	protected class OpenTreeRunnable implements Runnable {
@@ -262,11 +305,12 @@ abstract class LibraryBaseActivity extends ListActivity {
 		public void run() {
 			final Runnable postRunnable = new Runnable() {
 				public void run() {
-					startActivity(
+					startActivityForResult(
 						new Intent(LibraryBaseActivity.this, LibraryTreeActivity.class)
 							.putExtra(SELECTED_BOOK_PATH_KEY, mySelectedBookPath)
 							.putExtra(TREE_PATH_KEY, myTreePath)
-							.putExtra(PARAMETER_KEY, myParameter)
+							.putExtra(PARAMETER_KEY, myParameter),
+						CHILD_LIST_REQUEST
 					);
 				}
 			};

geometer/FBReaderJ-alex

(cherry picked from commit 1508c2658386717cc3b06954278f15797d5f8305 with conflicts)

src/org/geometerplus/android/fbreader/library/LibraryBaseActivity.java
@@ -22,6 +22,7 @@ package org.geometerplus.android.fbreader.library;
 import java.util.List;
 
 import android.app.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -65,12 +66,25 @@ abstract class LibraryBaseActivity extends ListActivity {
 	protected final ZLResource myResource = ZLResource.resource("libraryView");
 	protected String mySelectedBookPath;
 
+	private static int CHILD_LIST_REQUEST = 0;
+	private static int RESULT_DONT_INVALIDATE_VIEWS = 0;
+	private static int RESULT_INVALIDATE_VIEWS = 1;
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
+		setResult(RESULT_DONT_INVALIDATE_VIEWS);
 		Thread.setDefaultUncaughtExceptionHandler(new org.geometerplus.zlibrary.ui.android.library.UncaughtExceptionHandler(this));
 		mySelectedBookPath = getIntent().getStringExtra(SELECTED_BOOK_PATH_KEY);
 	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int returnCode, Intent intent) {
+		if (requestCode == CHILD_LIST_REQUEST && returnCode == RESULT_INVALIDATE_VIEWS) {
+			getListView().invalidateViews();
+			setResult(RESULT_INVALIDATE_VIEWS);
+		}
+	} 
 
 	@Override
 	public boolean onSearchRequested() {
@@ -221,24 +235,53 @@ abstract class LibraryBaseActivity extends ListActivity {
 		final int position = ((AdapterView.AdapterContextMenuInfo)item.getMenuInfo()).position;
 		final FBTree tree = ((LibraryAdapter)getListAdapter()).getItem(position);
 		if (tree instanceof BookTree) {
-			final BookTree bookTree = (BookTree)tree;
+			final Book book = ((BookTree)tree).Book;
 			switch (item.getItemId()) {
 				case OPEN_BOOK_ITEM_ID:
-					openBook(bookTree.Book);
+					openBook(book);
 					return true;
 				case ADD_TO_FAVORITES_ITEM_ID:
-					LibraryInstance.addBookToFavorites(bookTree.Book);
+					LibraryInstance.addBookToFavorites(book);
 					return true;
 				case REMOVE_FROM_FAVORITES_ITEM_ID:
-					LibraryInstance.removeBookFromFavorites(bookTree.Book);
+					LibraryInstance.removeBookFromFavorites(book);
 					getListView().invalidateViews();
 					return true;
 				case DELETE_BOOK_ITEM_ID:
-					// TODO: implement
+					tryToDeleteBook(book);
 					return true;
 			}
 		}
 		return super.onContextItemSelected(item);
+	}
+
+	private class BookDeleter implements DialogInterface.OnClickListener {
+		private final Book myBook;
+		private final int myMode;
+
+		BookDeleter(Book book, int removeMode) {
+			myBook = book;
+			myMode = removeMode;
+		}
+
+		public void onClick(DialogInterface dialog, int which) {
+			LibraryInstance.removeBook(myBook, myMode);
+			getListView().invalidateViews();
+			setResult(RESULT_INVALIDATE_VIEWS);
+		}
+	}
+
+	private void tryToDeleteBook(Book book) {
+		final ZLResource dialogResource = ZLResource.resource("dialog");
+		final ZLResource buttonResource = dialogResource.getResource("button");
+		final ZLResource boxResource = dialogResource.getResource("deleteBookBox");
+		new AlertDialog.Builder(this)
+			.setTitle(book.getTitle())
+			.setMessage(boxResource.getResource("message").getValue())
+			.setIcon(0)
+			.setPositiveButton(buttonResource.getResource("yes").getValue(), new BookDeleter(book, Library.REMOVE_FROM_DISK))
+			.setNegativeButton(buttonResource.getResource("no").getValue(), null)
+			.create().show();
 	}
 
 	protected class OpenTreeRunnable implements Runnable {
@@ -259,11 +302,12 @@ abstract class LibraryBaseActivity extends ListActivity {
 		public void run() {
 			final Runnable postRunnable = new Runnable() {
 				public void run() {
-					startActivity(
+					startActivityForResult(
 						new Intent(LibraryBaseActivity.this, LibraryTreeActivity.class)
 							.putExtra(SELECTED_BOOK_PATH_KEY, mySelectedBookPath)
 							.putExtra(TREE_PATH_KEY, myTreePath)
-							.putExtra(PARAMETER_KEY, myParameter)
+							.putExtra(PARAMETER_KEY, myParameter),
+						CHILD_LIST_REQUEST
 					);
 				}
 			};