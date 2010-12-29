9564710918467a94ad5e6b2d8f751d9b494c7952
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-12-29 17:24:59 +0300

    :library process should be killed before displaying a book (cherry picked from commit 34f33c2d630346bd51e96be20016212de79759d0)
    Conflicts:
    
    	src/org/geometerplus/android/fbreader/FBReader.java

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -57,7 +57,7 @@ import org.geometerplus.fbreader.fbreader.ActionCode;
 import org.geometerplus.android.fbreader.buttons.AbstractButton;
 import org.geometerplus.android.fbreader.buttons.ButtonsCollection;
 import org.geometerplus.android.fbreader.buttons.SQLiteButtonsDatabase;
-import org.geometerplus.android.fbreader.library.InitializationService;
+import org.geometerplus.android.fbreader.library.KillerService;
 import org.geometerplus.android.util.UIUtil;
 
 public final class FBReader extends ZLAndroidActivity {
@@ -265,7 +265,7 @@ public final class FBReader extends ZLAndroidActivity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		stopService(new Intent(getApplicationContext(), InitializationService.class));
+		startService(new Intent(getApplicationContext(), KillerService.class));
 		myEPDView.onResume();
 		ControlButtonPanel.restoreVisibilities();
 		myReadMode = true;
