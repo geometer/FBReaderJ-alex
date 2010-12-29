b780bb152d0a6a0a2fc5b616f59b68299acded01
Nikolay Pultsin <geometer@fbreader.org>
2010-12-29 12:37:23 +0000

    additional checks in activity onCreate; cleanup (cherry picked from commit 666925c1200cd3bc1f7d968d8d4b9c17942cbc52)

--- src/org/geometerplus/android/fbreader/library/FileManager.java
+++ src/org/geometerplus/android/fbreader/library/FileManager.java
@@ -54,6 +54,11 @@ public final class FileManager extends BaseActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		if (DatabaseInstance == null || LibraryInstance == null) {
+			finish();
+			return;
+		}
+
 		FileListAdapter adapter = new FileListAdapter();
 		setListAdapter(adapter);
 
