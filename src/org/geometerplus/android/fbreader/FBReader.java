bbd25ea60b45f1a4bdc1ce61e8a88aa93a71cef4
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-12-30 00:16:29 +0300

    KillerCallback instead of KillerService (cherry picked from commit ad9da693894318f52085b4ce8d713e5a22c5097c)
    Conflicts:
    
    	src/org/geometerplus/android/fbreader/FBReader.java
    	src/org/geometerplus/android/fbreader/library/KillerCallback.java

--- src/org/geometerplus/android/fbreader/FBReader.java
+++ src/org/geometerplus/android/fbreader/FBReader.java
@@ -57,7 +57,7 @@ import org.geometerplus.fbreader.fbreader.ActionCode;
 import org.geometerplus.android.fbreader.buttons.AbstractButton;
 import org.geometerplus.android.fbreader.buttons.ButtonsCollection;
 import org.geometerplus.android.fbreader.buttons.SQLiteButtonsDatabase;
-import org.geometerplus.android.fbreader.library.KillerService;
+import org.geometerplus.android.fbreader.library.KillerCallback;
 import org.geometerplus.android.util.UIUtil;
 
 public final class FBReader extends ZLAndroidActivity {
@@ -265,7 +265,7 @@ public final class FBReader extends ZLAndroidActivity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		startService(new Intent(getApplicationContext(), KillerService.class));
+		sendBroadcast(new Intent(getApplicationContext(), KillerCallback.class));
 		myEPDView.onResume();
 		ControlButtonPanel.restoreVisibilities();
 		myReadMode = true;
